9eaf7a901ed113db831fcc88876b9f4ee63119c1
stephan.diederich <stephan.diederich@a383d182-5b53-0410-acfb-4f71ec231b6c>
2008-10-26 12:43:36 +0000

    author adjustments, added licence

--- branches/testing/src/aucdialog.h
+++ branches/testing/src/aucdialog.h
@@ -1,6 +1,6 @@
 //   -*- c++ -*------------------------------------------------------------
 //
-//   Author:            Stephan Diederich
+//   Author:            Scott Davilla / Stephan Diederich
 //
 //   Project name:      atvusb-creator
 //
@@ -16,8 +16,21 @@
 //
 //   Description:       Implementation file for main ui dialog. uses platform specific AtvUsbCreatorBase implementation
 //
+//   Copyright (C) 2008  TeamXBMC
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public License
+//   as published by the Free Software Foundation; either version 2
+//   of the License, or (at your option) any later version.
+//   
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //   ---------------------------------------------------------------------- 
-
 
 #ifndef aucDialog_H
 #define aucDialog_H
